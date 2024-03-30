4d744d572d5f2e614e3ddd87dbde909a5020c29f
Bassem2504 <147065238+Bassem2504@users.noreply.github.com>
2024-03-30 04:20:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img1')
+function phones(phone) {
+    img.src = phone ;
+}
+
+let body = document.querySelector('body')
+function colors(color) {
+    body.style.background = color ;
+}
+
